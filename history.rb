63851f7369170d2170bae35d6c2a2a7830f3a1f2
Tony Bowden <tony@tmtm.com>
2017-10-16 17:58:04 +0100

    rubocop -a

--- history.rb
+++ history.rb
@@ -31,26 +31,29 @@ class Check
 
   def missing_fields
     expected = { start_date: 580 }
-    expected.merge!({ prev: 1365 }) if earlier
-    expected.merge!({ end_date: 582, next: 1366 }) if later
+    expected[:prev] = 1365 if earlier
+    if later
+      expected[:end_date] = 582
+      expected[:next] = 1366
+    end
     missing = expected.keys.reject { |i| current.send(i) }
     return unless missing.any?
-    return [ "Missing field#{missing.count > 1 ? 's' : ''}", "#{current.item} is missing #{missing.map { |i| "{{P|#{expected[i]}}}" }.join(", ")}" ]
+    ["Missing field#{missing.count > 1 ? 's' : ''}", "#{current.item} is missing #{missing.map { |i| "{{P|#{expected[i]}}}" }.join(', ')}"]
   end
 
   def ends_after_successor_starts
     return unless current.end_date && later&.start_date
-    return [ 'Date overlap', "#{current.item} has a {{P|580}} of #{current.end_date}, which is later than {{P|580}} of #{later.start_date} for #{later.item}" ] if current.end_date > later.start_date
+    return ['Date overlap', "#{current.item} has a {{P|580}} of #{current.end_date}, which is later than {{P|580}} of #{later.start_date} for #{later.item}"] if current.end_date > later.start_date
   end
 
   def wrong_predecessor
     return unless current.prev && earlier&.item
-    return [ 'Inconsistent predecessor', "#{current.item} has a {{P|1365}} of #{current.prev}, which differs from #{earlier.item}" ] if current.prev != earlier.item
+    return ['Inconsistent predecessor', "#{current.item} has a {{P|1365}} of #{current.prev}, which differs from #{earlier.item}"] if current.prev != earlier.item
   end
 
   def wrong_successor
     return unless current.next && later&.item
-    return [ 'Inconsistent sucessor', "#{current.item} has a {{P|1366}} of #{current.next}, which differs from #{later.item}" ] if current.next != later.item
+    return ['Inconsistent sucessor', "#{current.item} has a {{P|1366}} of #{current.next}, which differs from #{later.item}"] if current.next != later.item
   end
 
   attr_reader :later, :current, :earlier
@@ -64,8 +67,8 @@ rescue RestClient::Exception => e
 end
 
 def warning(check, method)
-  errors = check.send(method) or return ""
-  '<span style="display: block">[[File:Pictogram voting comment.svg|15px|link=]]&nbsp;<span style="color: #d33; font-weight: bold; vertical-align: middle;">%s</span>&nbsp;<ref>%s</ref></span>' % [ errors.first, errors.last ]
+  errors = check.send(method) or return ''
+  '<span style="display: block">[[File:Pictogram voting comment.svg|15px|link=]]&nbsp;<span style="color: #d33; font-weight: bold; vertical-align: middle;">%s</span>&nbsp;<ref>%s</ref></span>' % [errors.first, errors.last]
 end
 
 query = <<EOQ
@@ -93,10 +96,10 @@ list.each_cons(3) do |later, current, earlier|
   next unless current
   check = Check.new(later, current, earlier)
   puts '|-'
-  puts '| style="padding:1em" | %s' % [ current.ordinal ? "#{current.ordinal}." : '' ]
+  puts '| style="padding:1em" | %s' % [current.ordinal ? "#{current.ordinal}." : '']
   puts '| style="padding:1em" | <span style="font-size: 1.5em; display: block;">%s</span> %s %s' % [
     current.item, (current.start_date || current.end_date ? "#{current.start_date}–#{current.end_date}" : ''),
-    warning(check, :missing_fields) + warning(check, :wrong_predecessor) + warning(check, :wrong_successor) + warning(check, :ends_after_successor_starts)
+    warning(check, :missing_fields) + warning(check, :wrong_predecessor) + warning(check, :wrong_successor) + warning(check, :ends_after_successor_starts),
   ]
 end
 puts '|}'
