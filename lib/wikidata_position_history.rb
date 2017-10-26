84f973b8f9bc36a954cff0148aa073e072892916
Mark Longair <mhl@pobox.com>
2017-10-26 17:23:14 +0100

    Add the initializer and attributes of the PageRewriter class

--- lib/wikidata_position_history.rb
+++ lib/wikidata_position_history.rb
@@ -2,5 +2,13 @@ require "wikidata_position_history/version"
 require "wikidata_position_history/history"
 
 module WikidataPositionHistory
-  # Your code goes here...
+  class PageRewriter
+    def initialize(mediawiki_site:, page_title:)
+      @mediawiki_site = mediawiki_site
+      @page_title = page_title.gsub('_', ' ')
+    end
+
+    private
+    attr_reader :mediawiki_site, :page_title
+  end
 end
---
+++ test/wikidata_position_history/page_rewriter_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+describe 'WikidataPositionHistory' do
+  describe 'PageRewriter' do
+    subject {
+      WikidataPositionHistory::PageRewriter.new(
+        mediawiki_site: 'www.wikidata.org',
+        page_title: 'User:Mhl20/Prime_minister_test'
+      )
+    }
+
+    it 'should have a mediawiki_site attribute' do
+      subject.send(:mediawiki_site).must_equal 'www.wikidata.org'
+    end
+
+    it 'should have a page_title attribute, where underscores have been changed to spaces' do
+      subject.send(:page_title).must_equal 'User:Mhl20/Prime minister test'
+    end
+  end
+end
