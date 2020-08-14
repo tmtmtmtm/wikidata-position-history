706ff803d8d97d99ab4d6d8e7523918b783cc691
Tony Bowden <tony@tmtm.com>
2020-08-14 21:56:44 +0100

    rename history.rb as report.rb
    There are too many files called 'history'. Make it clearer that this one
    is the final report.
    
    (This also has too many classes inside it, but we can split those out
    as a separate step.)

--- lib/wikidata_position_history.rb
+++ lib/wikidata_position_history.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'wikidata_position_history/version'
-require 'wikidata_position_history/history'
+require 'wikidata_position_history/report'
 
 require 'date'
 
