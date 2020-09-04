99fc2c8e213667ef3abffe80a9d4000fecf90fbb
Tony Bowden <tony@tmtm.com>
2020-09-04 19:30:11 +0100

    Fix RSpec/LeakyConstantDeclaration warnings

--- test/wikidata_position_history/imprecise_dates_spec.rb
+++ test/wikidata_position_history/imprecise_dates_spec.rb
@@ -3,41 +3,39 @@
 require 'test_helper'
 
 describe WikidataPositionHistory::Report do
+  before { use_sample_data }
+  let(:rows) { WikidataPositionHistory::Report.new(position_id).wikitext_with_header.split('|-') }
+  let(:date_re) { /(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/ }
+
   describe 'Ambassador to Albania' do
-    before { use_sample_data }
-
-    let(:rows) { WikidataPositionHistory::Report.new('Q56761097').wikitext_with_header.split('|-') }
-
-    DATE_RE = /(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/.freeze
+    let(:position_id) { 'Q56761097' }
 
     it 'handles precision 11 dates' do
       holder = rows.find { |line| line.include? 'Q507012' }
-      dates = holder.match(DATE_RE).named_captures
+      dates = holder.match(date_re).named_captures
       expect(dates['start']).must_equal '1936-09-25'
     end
 
     it 'handles precision 10 dates' do
       holder = rows.find { |line| line.include? 'Q7326923' }
-      dates = holder.match(DATE_RE).named_captures
+      dates = holder.match(date_re).named_captures
       expect(dates['start']).must_equal '2003-10'
     end
 
     it 'handles precision 9 dates' do
       holder = rows.find { |line| line.include? 'Q56849411' }
-      dates = holder.match(DATE_RE).named_captures
+      dates = holder.match(date_re).named_captures
       expect(dates['start']).must_equal '2012'
     end
 
     it 'handles precisions in inception dates' do
-      dates = rows.first.match(DATE_RE).named_captures
+      dates = rows.first.match(date_re).named_captures
       expect(dates['start']).must_equal '1922'
     end
   end
 
   describe 'Governor of Gibralatar' do
-    before { use_sample_data }
-
-    let(:rows) { WikidataPositionHistory::Report.new('Q195965').wikitext_with_header.split('|-') }
+    let(:position_id) { 'Q195965' }
 
     it 'does not display date line when there are no dates' do
       holder = rows.find { |line| line.include? 'Q16859171' }
