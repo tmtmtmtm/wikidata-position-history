8066a305411408726194872e1823d82ca3221a2d
Tony Bowden <tony@tmtm.com>
2020-09-04 19:07:16 +0100

    Fix RSpec/MultipleDescribes warning

--- test/wikidata_position_history/imprecise_dates_spec.rb
+++ test/wikidata_position_history/imprecise_dates_spec.rb
@@ -2,51 +2,53 @@
 
 require 'test_helper'
 
-describe 'Ambassador to Albania' do
-  before { use_sample_data }
+describe WikidataPositionHistory::Report do
+  describe 'Ambassador to Albania' do
+    before { use_sample_data }
 
-  let(:rows) { WikidataPositionHistory::Report.new('Q56761097').wikitext_with_header.split('|-') }
+    let(:rows) { WikidataPositionHistory::Report.new('Q56761097').wikitext_with_header.split('|-') }
 
-  DATE_RE = /(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/.freeze
+    DATE_RE = /(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/.freeze
 
-  it 'handles precision 11 dates' do
-    holder = rows.find { |line| line.include? 'Q507012' }
-    dates = holder.match(DATE_RE).named_captures
-    expect(dates['start']).must_equal '1936-09-25'
+    it 'handles precision 11 dates' do
+      holder = rows.find { |line| line.include? 'Q507012' }
+      dates = holder.match(DATE_RE).named_captures
+      expect(dates['start']).must_equal '1936-09-25'
+    end
+
+    it 'handles precision 10 dates' do
+      holder = rows.find { |line| line.include? 'Q7326923' }
+      dates = holder.match(DATE_RE).named_captures
+      expect(dates['start']).must_equal '2003-10'
+    end
+
+    it 'handles precision 9 dates' do
+      holder = rows.find { |line| line.include? 'Q56849411' }
+      dates = holder.match(DATE_RE).named_captures
+      expect(dates['start']).must_equal '2012'
+    end
+
+    it 'handles precisions in inception dates' do
+      dates = rows.first.match(DATE_RE).named_captures
+      expect(dates['start']).must_equal '1922'
+    end
   end
 
-  it 'handles precision 10 dates' do
-    holder = rows.find { |line| line.include? 'Q7326923' }
-    dates = holder.match(DATE_RE).named_captures
-    expect(dates['start']).must_equal '2003-10'
-  end
+  describe 'Governor of Gibralatar' do
+    before { use_sample_data }
 
-  it 'handles precision 9 dates' do
-    holder = rows.find { |line| line.include? 'Q56849411' }
-    dates = holder.match(DATE_RE).named_captures
-    expect(dates['start']).must_equal '2012'
-  end
+    let(:rows) { WikidataPositionHistory::Report.new('Q195965').wikitext_with_header.split('|-') }
 
-  it 'handles precisions in inception dates' do
-    dates = rows.first.match(DATE_RE).named_captures
-    expect(dates['start']).must_equal '1922'
-  end
-end
+    it 'does not display date line when there are no dates' do
+      holder = rows.find { |line| line.include? 'Q16859171' }
+      expect(holder).wont_include '–'
+    end
 
-describe 'Governor of Gibralatar' do
-  before { use_sample_data }
-
-  let(:rows) { WikidataPositionHistory::Report.new('Q195965').wikitext_with_header.split('|-') }
-
-  it 'does not display date line when there are no dates' do
-    holder = rows.find { |line| line.include? 'Q16859171' }
-    expect(holder).wont_include '–'
-  end
-
-  it 'does not display date precision warnings when there are no dates' do
-    # Robert Napier as the first 'dateless' entry is notionally followed
-    # by Colin Campbell, the first 'dated' entry.
-    holder = rows.find { |line| line.include? 'Q336474' }
-    expect(holder).wont_include 'Date precision'
+    it 'does not display date precision warnings when there are no dates' do
+      # Robert Napier as the first 'dateless' entry is notionally followed
+      # by Colin Campbell, the first 'dated' entry.
+      holder = rows.find { |line| line.include? 'Q336474' }
+      expect(holder).wont_include 'Date precision'
+    end
   end
 end
