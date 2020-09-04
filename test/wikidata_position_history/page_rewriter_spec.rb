f27fdc8a946c6e5df5ca802a8a2123898bcd1749
Tony Bowden <tony@tmtm.com>
2020-09-04 18:58:42 +0100

    Fix RSpec/ExampleWording warnings
    Spec names should not start with 'should'

--- test/wikidata_position_history/page_rewriter_spec.rb
+++ test/wikidata_position_history/page_rewriter_spec.rb
@@ -13,15 +13,15 @@ describe 'WikidataPositionHistory' do
       )
     end
 
-    it 'should have a mediawiki_site attribute' do
+    it 'has a mediawiki_site attribute' do
       expect(page.send(:mediawiki_site)).must_equal 'www.wikidata.org'
     end
 
-    it 'should have a page_title attribute, where underscores have been changed to spaces' do
+    it 'has a page_title attribute, where underscores have been changed to spaces' do
       expect(page.send(:page_title)).must_equal 'User:Mhl20/Prime minister test'
     end
 
-    it 'should error with no itemid' do
+    it 'errors with no itemid' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory}}')
 
@@ -29,14 +29,14 @@ describe 'WikidataPositionHistory' do
       expect(page.send(:new_content).last).must_equal 'The id parameter was missing'
     end
 
-    it 'should error with invalid itemid' do
+    it 'errors with invalid itemid' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory|id=Prime Minister}}')
 
       expect(page.new_content.last).must_equal 'The id parameter was malformed'
     end
 
-    it 'should get a well-formed position ID' do
+    it 'gets a well-formed position ID' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory|id=Q1769526}}')
 
@@ -45,7 +45,7 @@ describe 'WikidataPositionHistory' do
   end
 
   describe 'DerivedIDs' do
-    it 'should derive an item from the Page where none supplied' do
+    it 'derives an item from the Page where none supplied' do
       stub_request(:get, /wikidata.org.*Talk:Q42/)
         .to_return(body: '{{PositionHolderHistory}}')
 
@@ -55,7 +55,7 @@ describe 'WikidataPositionHistory' do
       ).send(:position_id)).must_equal 'Q42'
     end
 
-    it 'should take the last ID from the Page name if multiple options' do
+    it 'takes the last ID from the Page name if multiple options' do
       stub_request(:get, %r{wikidata.org.*Talk:Q42/Q1769526})
         .to_return(body: '{{PositionHolderHistory}}')
 
@@ -65,7 +65,7 @@ describe 'WikidataPositionHistory' do
       ).send(:position_id)).must_equal 'Q1769526'
     end
 
-    it 'should prefer the ID in the template, even if one is in the URL' do
+    it 'prefers the ID in the template, even if one is in the URL' do
       stub_request(:get, /wikidata.org.*Talk:Q42/)
         .to_return(body: '{{PositionHolderHistory|id=Q1769526}}')
 
