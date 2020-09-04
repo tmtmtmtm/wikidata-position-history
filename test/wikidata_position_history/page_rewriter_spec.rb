be769304214b6c4eec0cac75a4609082272952ea
Tony Bowden <tony@tmtm.com>
2020-09-04 18:56:38 +0100

    Don't used an unnamed 'subject'
    RSpec allows named subjects, though it doesn't seem like minitest does.
    Using `let` blocks appears to give the best of both.

--- test/wikidata_position_history/page_rewriter_spec.rb
+++ test/wikidata_position_history/page_rewriter_spec.rb
@@ -6,7 +6,7 @@ describe 'WikidataPositionHistory' do
   before { fake_login }
 
   describe 'PrimeMinisterTest' do
-    subject do
+    let(:page) do
       WikidataPositionHistory::PageRewriter.new(
         mediawiki_site: 'www.wikidata.org',
         page_title:     'User:Mhl20/Prime_minister_test'
@@ -14,33 +14,33 @@ describe 'WikidataPositionHistory' do
     end
 
     it 'should have a mediawiki_site attribute' do
-      expect(subject.send(:mediawiki_site)).must_equal 'www.wikidata.org'
+      expect(page.send(:mediawiki_site)).must_equal 'www.wikidata.org'
     end
 
     it 'should have a page_title attribute, where underscores have been changed to spaces' do
-      expect(subject.send(:page_title)).must_equal 'User:Mhl20/Prime minister test'
+      expect(page.send(:page_title)).must_equal 'User:Mhl20/Prime minister test'
     end
 
     it 'should error with no itemid' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory}}')
 
-      expect(subject.send(:position_id)).must_be_empty
-      expect(subject.send(:new_content).last).must_equal 'The id parameter was missing'
+      expect(page.send(:position_id)).must_be_empty
+      expect(page.send(:new_content).last).must_equal 'The id parameter was missing'
     end
 
     it 'should error with invalid itemid' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory|id=Prime Minister}}')
 
-      expect(subject.new_content.last).must_equal 'The id parameter was malformed'
+      expect(page.new_content.last).must_equal 'The id parameter was malformed'
     end
 
     it 'should get a well-formed position ID' do
       stub_request(:get, /wikidata.org.*Prime%20minister%20test/)
         .to_return(body: '{{PositionHolderHistory|id=Q1769526}}')
 
-      expect(subject.send(:position_id)).must_equal 'Q1769526'
+      expect(page.send(:position_id)).must_equal 'Q1769526'
     end
   end
 
