d95a65bf5cc39dc77350ddec30b2a5cffd5f43a4
Tony Bowden <tony@tmtm.com>
2020-09-06 08:58:33 +0100

    refactor dates_spec test
    Extract most of the boilerplate so it's more obvious what each spec is
    doing.

--- test/wikidata_position_history/report/dates_spec.rb
+++ test/wikidata_position_history/report/dates_spec.rb
@@ -6,48 +6,52 @@ describe WikidataPositionHistory::Report do
   before { use_sample_data }
 
   let(:rows) { WikidataPositionHistory::Report.new(position_id).wikitext_with_header.split('|-') }
-  let(:date_re) { /(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/ }
+  let(:holder_row) { rows.find { |line| line.include? officeholder } }
+  let(:dates) { holder_row.match(/(?<start>[\d\-]+)\s–\s(?<end>[\d-]+)?/).named_captures }
 
   describe 'Ambassador to Albania' do
     let(:position_id) { 'Q56761097' }
 
-    it 'handles precision 11 dates' do
-      holder = rows.find { |line| line.include? 'Q507012' }
-      dates = holder.match(date_re).named_captures
-      expect(dates['start']).must_equal '1936-09-25'
+    describe 'precision 11 dates' do
+      let(:officeholder) { 'Q507012' }
+
+      it { expect(dates['start']).must_equal '1936-09-25' }
     end
 
-    it 'handles precision 10 dates' do
-      holder = rows.find { |line| line.include? 'Q7326923' }
-      dates = holder.match(date_re).named_captures
-      expect(dates['start']).must_equal '2003-10'
+    describe 'precision 10 dates' do
+      let(:officeholder) { 'Q7326923' }
+
+      it { expect(dates['start']).must_equal '2003-10' }
     end
 
-    it 'handles precision 9 dates' do
-      holder = rows.find { |line| line.include? 'Q56849411' }
-      dates = holder.match(date_re).named_captures
-      expect(dates['start']).must_equal '2012'
+    describe 'precision 9 dates' do
+      let(:officeholder) { 'Q56849411' }
+
+      it { expect(dates['start']).must_equal '2012' }
     end
 
-    it 'handles precisions in inception dates' do
-      dates = rows.first.match(date_re).named_captures
-      expect(dates['start']).must_equal '1922'
+    describe 'precisions in position inception dates' do
+      let(:holder_row) { rows.first }
+
+      it { expect(dates['start']).must_equal '1922' }
     end
   end
 
   describe 'Governor of Gibralatar' do
     let(:position_id) { 'Q195965' }
 
-    it 'does not display date line when there are no dates' do
-      holder = rows.find { |line| line.include? 'Q16859171' }
-      expect(holder).wont_include '–'
+    describe 'no date line when no dates' do
+      let(:officeholder) { 'Q16859171' }
+
+      it { expect(holder_row).wont_include '–' }
     end
 
-    it 'does not display date precision warnings when there are no dates' do
+    describe 'no precision warnings when no dates' do
       # Robert Napier as the first 'dateless' entry is notionally followed
       # by Colin Campbell, the first 'dated' entry.
-      holder = rows.find { |line| line.include? 'Q336474' }
-      expect(holder).wont_include 'Date precision'
+      let(:officeholder) { 'Q336474' }
+
+      it { expect(holder_row).wont_include 'Date precision' }
     end
   end
 end
