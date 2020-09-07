2f79e9282a380ddfea1ba5270dbd7d2f9f5c546d
Tony Bowden <tony@tmtm.com>
2020-09-07 08:01:15 +0100

    Add more full-output tests
    The idea is that these can become a full suite of acceptance tests for
    known tricky cases, with the unit tests moving towards testing lower
    level functionality.
    
    These will require more maintenance as the wider functionality changes,
    and will likely be refreshed fairly regularly. As the underlying data in
    Wikidata may have changed significantly in the meantime, these shouldn't
    be relied on to cover any specific regressions.

--- test/wikidata_position_history/report/wikitext_spec.rb
+++ test/wikidata_position_history/report/wikitext_spec.rb
@@ -2,18 +2,38 @@
 
 require 'test_helper'
 
-# This is just a very high level acceptance test at the moment - not
-# very useful for locating where a bug might be, but somewhat
-# helpful as a test of whether something's broken.
+# This is a very high level acceptance test to check that the full
+# output for a position is as expected.
+#
+# More detailed tests should hopefully uncover the specifics of any
+# problem: this is a fallback to catch anything slipping through.
+#
+# These will require more maintenance than other tests: the expectation
+# is that the source data and expected output *should* be regenerated
+# from time to time.
 
 describe WikidataPositionHistory::Report do
   before { use_sample_data }
 
-  let(:report)   { WikidataPositionHistory::Report.new('Q14211') }
-  let(:expected) { Pathname.new('test/Q14211.out').read }
+  let(:report)   { WikidataPositionHistory::Report.new(id) }
+  let(:pathname) { Pathname.new("test/expected-output/#{id}.out") }
+  let(:expected) { pathname.read }
 
-  it 'generates wikitext that starts how we expect' do
-    # binding.pry if subject.wikitext_with_header != expected
-    expect(report.wikitext_with_header).must_equal expected
+  describe 'Prime Minister of the United Kingdom' do
+    let(:id) { 'Q14211' }
+
+    it { expect(report.wikitext_with_header).must_equal expected }
+  end
+
+  describe 'Ambassador to Albania' do
+    let(:id) { 'Q56761097' }
+
+    it { expect(report.wikitext_with_header).must_equal expected }
+  end
+
+  describe 'Bishop of Worcester' do
+    let(:id) { 'Q1837494' }
+
+    it { expect(report.wikitext_with_header).must_equal expected }
   end
 end
