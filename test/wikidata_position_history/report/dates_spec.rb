290cbe277e62b89f6497bc4b094d563ce95b27f4
Tony Bowden <tony@tmtm.com>
2020-09-07 08:04:10 +0100

    Extract QueryService::WikidataDate unit tests
    These should be tested by interacting directly with the class, rather
    than indirectly via the eventual report.
    
    The checks that the full reports for these positions look correct have
    also been moved to wikidata_position_history/report/wikitext_spec.rb

--- test/wikidata_position_history/report/dates_spec.rb
+++ test/wikidata_position_history/report/dates_spec.rb
@@ -9,51 +9,6 @@ describe WikidataPositionHistory::Report do
   let(:holder_row) { rows.find { |line| line.include? "#{officeholder}}}</span>" } }
   let(:dates) { holder_row.match(/(?<start>[\ds\-]+)\s–\s(?<end>[\ds-]+)?/).named_captures }
 
-  describe 'Ambassador to Albania' do
-    let(:position_id) { 'Q56761097' }
-
-    describe 'precision 11 dates' do
-      let(:officeholder) { 'Q507012' }
-
-      it { expect(dates['start']).must_equal '1936-09-25' }
-    end
-
-    describe 'precision 10 dates' do
-      let(:officeholder) { 'Q7326923' }
-
-      it { expect(dates['start']).must_equal '2003-10' }
-    end
-
-    describe 'precision 9 dates' do
-      let(:officeholder) { 'Q56849411' }
-
-      it { expect(dates['start']).must_equal '2012' }
-    end
-
-    describe 'precisions in position inception dates' do
-      let(:holder_row) { rows.first }
-
-      it { expect(dates['start']).must_equal '1922' }
-    end
-  end
-
-  describe 'Bishop of Worcester' do
-    let(:position_id) { 'Q1837494' }
-
-    describe 'pre-1000 dates' do
-      let(:officeholder) { 'Q1273281' }
-
-      it { expect(dates['start']).must_equal '961' }
-      it { expect(dates['end']).must_equal '992-03-05' }
-    end
-
-    describe 'decade-precision dates' do
-      let(:officeholder) { 'Q6860722' }
-
-      it { expect(dates['start']).must_equal '740s' }
-    end
-  end
-
   describe 'Governor of Gibralatar' do
     let(:position_id) { 'Q195965' }
 
