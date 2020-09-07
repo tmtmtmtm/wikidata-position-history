3124fd3786a47cdda8b23274df437b84c6e3a86c
Tony Bowden <tony@tmtm.com>
2020-09-07 08:04:10 +0100

    Extract QueryService::WikidataDate unit tests
    These should be tested by interacting directly with the class, rather
    than indirectly via the eventual report.
    
    The checks that the full reports for these positions look correct have
    also been moved to wikidata_position_history/report/wikitext_spec.rb

---
+++ test/query_service/wikidata_date_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe QueryService::WikidataDate do
+  describe 'precision 11 dates' do
+    let(:date) { QueryService::WikidataDate.new('1936-09-25', '11') }
+
+    it { expect(date.to_s).must_equal '1936-09-25' }
+  end
+
+  describe 'precision 10 dates' do
+    let(:date) { QueryService::WikidataDate.new('1936-09-01', '10') }
+
+    it { expect(date.to_s).must_equal '1936-09' }
+  end
+
+  describe 'precision 09 dates' do
+    let(:date) { QueryService::WikidataDate.new('1936-01-01', '9') }
+
+    it { expect(date.to_s).must_equal '1936' }
+  end
+
+  describe 'precision 08 dates' do
+    let(:date) { QueryService::WikidataDate.new('1930-01-01', '8') }
+
+    it { expect(date.to_s).must_equal '1930s' }
+  end
+
+  describe 'pre-1000 precision 11 dates' do
+    let(:date) { QueryService::WikidataDate.new('0936-09-25', '11') }
+
+    it { expect(date.to_s).must_equal '936-09-25' }
+  end
+
+  describe 'pre-1000 precision 9 dates' do
+    let(:date) { QueryService::WikidataDate.new('0936-01-01', '9') }
+
+    it { expect(date.to_s).must_equal '936' }
+  end
+end
