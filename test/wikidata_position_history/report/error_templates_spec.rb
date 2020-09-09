74a1048bd84cf7bb71a769cfd59d7121c76c0385
Tony Bowden <tony@tmtm.com>
2020-09-09 07:06:20 +0100

    Bail out early if the position is legislative
    If the position in a subclass of a legislator, don't try to generate the
    report, which will have potentially tens of thousands of position
    holders, all with overlapping mandates, etc., but instead display an
    error template.

--- test/wikidata_position_history/report/error_templates_spec.rb
+++ test/wikidata_position_history/report/error_templates_spec.rb
@@ -9,4 +9,9 @@ describe WikidataPositionHistory::Report do
     result = WikidataPositionHistory::Report.new('Q39055044').wikitext
     expect(result[/{{(.*?)}}/, 1]).must_include 'error_no_holders'
   end
+
+  it 'outputs a warning template when the position is legislative' do
+    result = WikidataPositionHistory::Report.new('Q13653224').wikitext
+    expect(result[/{{(.*?)}}/, 1]).must_include 'error_legislator'
+  end
 end
