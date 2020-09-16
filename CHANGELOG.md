ea9a9032663422fc8b898b1cb84b75efb552520b
Tony Bowden <tony@tmtm.com>
2020-09-16 10:29:31 +0100

    Release 2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-# Unreleased
+# [2.0.0] 2020-09-16
 
 ## Interface change
 
--- lib/wikidata_position_history/version.rb
+++ lib/wikidata_position_history/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module WikidataPositionHistory
-  VERSION = '1.11.0'
+  VERSION = '2.0.0'
 end
