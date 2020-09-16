32e46a24c5cd868fd3ccb53fa2969b64cdc006cd
Tony Bowden <tony@tmtm.com>
2020-09-16 06:50:34 +0100

    Add ConstituencyMandatesQuery
    This is almost identical to the normal MandatesQuery, other than needing
    to look at a P768 (electoral district) qualifier. It is likely that we
    will want to change things about it later, however, so there's little
    value in trying to abstract out any of the commonality just yet.

--- lib/sparql/mandates_query.rb
+++ lib/sparql/mandates_query.rb
@@ -25,6 +25,30 @@ module WikidataPositionHistory
         SPARQL
       end
     end
+
+    # SPARQL for fetching all mandates of a single-member district
+    class ConstituencyMandatesQuery < ItemQuery
+      def raw_sparql
+        <<~SPARQL
+          # constituency-mandates
+
+          SELECT DISTINCT ?ordinal ?item ?start_date ?start_precision ?end_date ?end_precision ?prev ?next ?nature
+          WHERE {
+            ?item wdt:P31 wd:Q5 ; p:P39 ?posn .
+            ?posn pq:P768 wd:%s .
+            FILTER NOT EXISTS { ?posn wikibase:rank wikibase:DeprecatedRank }
+
+            OPTIONAL { ?posn pqv:P580 [ wikibase:timeValue ?start_date; wikibase:timePrecision ?start_precision ] }
+            OPTIONAL { ?posn pqv:P582 [ wikibase:timeValue ?end_date; wikibase:timePrecision ?end_precision ] }
+            OPTIONAL { ?posn pq:P1365|pq:P155 ?prev }
+            OPTIONAL { ?posn pq:P1366|pq:P156 ?next }
+            OPTIONAL { ?posn pq:P1545 ?ordinal }
+            OPTIONAL { ?posn pq:P5102 ?nature }
+          }
+          ORDER BY DESC(?start_date) ?item
+        SPARQL
+      end
+    end
   end
 
   # Represents a single row returned from the Mandates query
