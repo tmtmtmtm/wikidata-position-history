7fbe8e6d2b50a5195270c2d5ef5957350bba2fa0
Tony Bowden <tony@tmtm.com>
2020-09-20 07:26:02 +0100

    Also fetch the statement for mandate queries
    This will be unique per P39 claim, even if multiples of a
    single qualifier caused a potential combinatorial explosion in the
    resultset. Fetching this will allow for grouping by a single statement
    in code (which should be simpler than trying to do it in the SPARQL
    itself).

--- lib/sparql/mandates_query.rb
+++ lib/sparql/mandates_query.rb
@@ -8,18 +8,18 @@ module WikidataPositionHistory
         <<~SPARQL
           # position-mandates
 
-          SELECT DISTINCT ?ordinal ?item ?start_date ?start_precision ?end_date ?end_precision ?prev ?next ?nature
+          SELECT DISTINCT ?statement ?ordinal ?item ?start_date ?start_precision ?end_date ?end_precision ?prev ?next ?nature
           WHERE {
-            ?item wdt:P31 wd:Q5 ; p:P39 ?posn .
-            ?posn ps:P39 wd:%s .
-            FILTER NOT EXISTS { ?posn wikibase:rank wikibase:DeprecatedRank }
+            ?item wdt:P31 wd:Q5 ; p:P39 ?statement .
+            ?statement ps:P39 wd:%s .
+            FILTER NOT EXISTS { ?statement wikibase:rank wikibase:DeprecatedRank }
 
-            OPTIONAL { ?posn pqv:P580 [ wikibase:timeValue ?start_date; wikibase:timePrecision ?start_precision ] }
-            OPTIONAL { ?posn pqv:P582 [ wikibase:timeValue ?end_date; wikibase:timePrecision ?end_precision ] }
-            OPTIONAL { ?posn pq:P1365|pq:P155 ?prev }
-            OPTIONAL { ?posn pq:P1366|pq:P156 ?next }
-            OPTIONAL { ?posn pq:P1545 ?ordinal }
-            OPTIONAL { ?posn pq:P5102 ?nature }
+            OPTIONAL { ?statement pqv:P580 [ wikibase:timeValue ?start_date; wikibase:timePrecision ?start_precision ] }
+            OPTIONAL { ?statement pqv:P582 [ wikibase:timeValue ?end_date; wikibase:timePrecision ?end_precision ] }
+            OPTIONAL { ?statement pq:P1365|pq:P155 ?prev }
+            OPTIONAL { ?statement pq:P1366|pq:P156 ?next }
+            OPTIONAL { ?statement pq:P1545 ?ordinal }
+            OPTIONAL { ?statement pq:P5102 ?nature }
           }
           ORDER BY DESC(?start_date) ?item
         SPARQL
@@ -32,19 +32,19 @@ module WikidataPositionHistory
         <<~SPARQL
           # constituency-mandates
 
-          SELECT DISTINCT ?ordinal ?item ?start_date ?start_precision ?end_date ?end_precision ?party ?prev ?next ?term
+          SELECT DISTINCT ?statement ?ordinal ?item ?start_date ?start_precision ?end_date ?end_precision ?party ?prev ?next ?term
           WHERE {
-            ?item wdt:P31 wd:Q5 ; p:P39 ?posn .
-            ?posn pq:P768 wd:%s .
-            FILTER NOT EXISTS { ?posn wikibase:rank wikibase:DeprecatedRank }
+            ?item wdt:P31 wd:Q5 ; p:P39 ?statement .
+            ?statement pq:P768 wd:%s .
+            FILTER NOT EXISTS { ?statement wikibase:rank wikibase:DeprecatedRank }
 
-            OPTIONAL { ?posn pqv:P580 [ wikibase:timeValue ?start_date; wikibase:timePrecision ?start_precision ] }
-            OPTIONAL { ?posn pqv:P582 [ wikibase:timeValue ?end_date; wikibase:timePrecision ?end_precision ] }
-            OPTIONAL { ?posn pq:P1365 ?prev }
-            OPTIONAL { ?posn pq:P1366 ?next }
-            OPTIONAL { ?posn pq:P4100 ?party }
+            OPTIONAL { ?statement pqv:P580 [ wikibase:timeValue ?start_date; wikibase:timePrecision ?start_precision ] }
+            OPTIONAL { ?statement pqv:P582 [ wikibase:timeValue ?end_date; wikibase:timePrecision ?end_precision ] }
+            OPTIONAL { ?statement pq:P1365 ?prev }
+            OPTIONAL { ?statement pq:P1366 ?next }
+            OPTIONAL { ?statement pq:P4100 ?party }
             OPTIONAL {
-              ?posn pq:P2937 ?term .
+              ?statement pq:P2937 ?term .
               OPTIONAL { ?term p:P31/pq:P1545 ?ordinal }
             }
           }
@@ -56,6 +56,10 @@ module WikidataPositionHistory
 
   # Represents a single row returned from the Mandates query
   class MandateRow < SPARQL::QueryRow
+    def statement
+      raw(:statement)
+    end
+
     def ordinal
       raw(:ordinal)
     end
